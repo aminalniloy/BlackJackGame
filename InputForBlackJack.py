07c6a0c650aff431a724619e5aaac7dc33ce8326
Al Amin <108260763+aminalniloy@users.noreply.github.com>
2023-04-06 13:07:38 +0300

    Add files via upload

---
+++ InputForBlackJack.py
@@ -0,0 +1,16 @@
+#\u2660 = ♠ Spade
+#\u2663 = ♣ Club
+#\u2665 = ♥ Heart
+#\u2666 = ♦ Diamond
+
+
+Rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+Suit = ['♠', '♣', '♥', '♦']
+
+OutputFilePath = "BlackJack.txt"
+ofh = open(OutputFilePath, "w", encoding='utf-8')
+
+for s in Suit:
+	for r in Rank:
+		card = r + s + "\n"
+		ofh.write(card)
